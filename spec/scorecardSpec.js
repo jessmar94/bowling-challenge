19c1ff5d0847ecdad8f687611aade0bdf41d1f2b
Jessica Marais <jessica_marais@hotmail.com>
2019-10-26 19:19:51 +0200

    User can see total score for gutter game

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -6,7 +6,7 @@ describe("Scorecard", function() {
 
   beforeEach(function() {
     card = new Scorecard;
-    frame = new Frame; 
+    frame = new Frame;
   })
 
   describe('initialization', function() {
@@ -14,7 +14,34 @@ describe("Scorecard", function() {
       expect(card.frames).toEqual({ 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[], 10:[]})
     })
     it('starts with a frameCount of 1', function() {
-      expect(card.frameCount).toEqual(1)
+      expect(card.frameKeyNumber).toEqual(1)
+    })
+
+    it('starts with a total score of 0', function() {
+      expect(card.totalScore).toEqual(0)
+    })
+  })
+
+  describe('initFrames', function() {
+    it('pushes a frame into the frames hash', function() {
+      frame = [5,2]
+      card.initFrames(frame)
+      expect(card.frames).toEqual({ 1:[5, 2], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[], 10:[]})
+    })
+  })
+
+  describe('total score for gutter game', function() {
+    it('returns a total score of 0', function() {
+      let frames = []
+      for (var i = 0; i < 10; i++) {
+        frames[i] = new Frame()
+        frames[i].bowl(0)
+        frames[i].bowl(0)
+      }
+      for (var i = 0; i < 10; i++) {
+        card.initFrames(frames[i])
+      }
+      expect(card.getTotalScore()).toEqual(0)
     })
   })
 })
--- src/scorecard.js
+++ src/scorecard.js
@@ -2,5 +2,18 @@
 
 function Scorecard() {
   this.frames = { 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[], 10:[] };
-  this.frameCount = 1
+  this.frameKeyNumber = 1
+  this.totalScore = 0
+}
+
+Scorecard.prototype.initFrames = function(frame) {
+  this.frames[this.frameKeyNumber] = frame;
+  this.frameKeyNumber++;
+}
+
+Scorecard.prototype.getTotalScore = function() {
+  for (var i = 1; i < 11; i++) {
+    this.totalScore += this.frames[i].frameScore;
+  }
+  return this.totalScore
 }

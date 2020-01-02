76e0722c41057bf3c6f34aea12f6fac3771148cd
Jessica Marais <jessica_marais@hotmail.com>
2020-01-02 15:30:48 +0000

    Clean up bowl and framescore methods

--- src/frame.js
+++ src/frame.js
@@ -1,26 +1,20 @@
 "use strict";
 
 function Frame() {
-  this.frameScore = 0
   this.roll = [];
   this.strike = false;
   this.spare = false;
 }
 
-Frame.prototype.getFrameScore = function() {
-  this.frameScore = this.calcFrameScore()
-  return this.frameScore
+Frame.prototype.bowl = function(knockedPins) {
+  if (knockedPins <= 10 && this.getFrameScore(knockedPins) <= 10 && this.validateFrameLength()) {
+    this.roll.push(knockedPins)
+  }
+  return this.getFrameScore();
 }
 
-Frame.prototype.bowl = function(knockedPins) {
-  if (knockedPins <= 10 && this.calcFrameScore(knockedPins) <= 10 && this.validateFrameLength()) {
-    this.roll.push(knockedPins)
-    this.isStrike()
-    this.isSpare()
-    return this.getFrameScore()
-  } else {
-    return this.getFrameScore()
-  }
+Frame.prototype.getFrameScore = function() {
+  return this.roll.reduce((a, b) => a + b, num)
 }
 
 Frame.prototype.validateFrameLength = function() {
@@ -29,10 +23,6 @@ Frame.prototype.validateFrameLength = function() {
   } else {
     return true
   }
-}
-
-Frame.prototype.calcFrameScore = function(num=0) {
-  return this.roll.reduce((a, b) => a + b, num)
 }
 
 Frame.prototype.isStrike = function() {

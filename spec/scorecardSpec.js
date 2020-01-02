9ff4c41ddc8d3d7c36002af6a7f08e646e66bfd7
Jessica Marais <jessica_marais@hotmail.com>
2020-01-02 18:50:08 +0000

    Linter changes

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -1,58 +1,58 @@
 const Frame = require('../src/frame');
 const Scorecard = require('../src/scorecard');
 
-describe("Scorecard", function() {
-  let card
+describe('Scorecard', () => {
+  let card;
 
-  beforeEach(function() {
+  beforeEach(() => {
     card = new Scorecard(Frame);
-  })
+  });
 
-  describe('gutter game', function() {
-    it('returns a total score of 0 for gutter game', function() {
+  describe('gutter game', () => {
+    it('returns a total score of 0 for gutter game', () => {
       for (let i = 0; i < 10; i++) {
-        card.frames[i].bowl(0)
-        card.frames[i].bowl(0)
+        card.frames[i].bowl(0);
+        card.frames[i].bowl(0);
       }
-      expect(card.getCumulativeScore()).toEqual(0)
-    })
-  })
+      expect(card.getCumulativeScore()).toEqual(0);
+    });
+  });
 
-  describe('total score', function() {
-    it('returns total score when no strike or spare', function() {
+  describe('total score', () => {
+    it('returns total score when no strike or spare', () => {
       for (let i = 0; i < 10; i++) {
-        card.frames[i] = new Frame()
-        card.frames[i].bowl(4)
-        card.frames[i].bowl(4)
+        card.frames[i] = new Frame();
+        card.frames[i].bowl(4);
+        card.frames[i].bowl(4);
       }
-      expect(card.getTotalScore()).toEqual(80)
-    })
-  })
+      expect(card.getTotalScore()).toEqual(80);
+    });
+  });
 
-  describe('a perfect game', function(){
-    it('shows a total score of 300 for a perfect game', function(){
+  describe('a perfect game', () => {
+    it('shows a total score of 300 for a perfect game', () => {
       for (let i = 0; i < 10; i++) {
-        card.frames[i].bowl(10)
-        card.frames[i].bowl(0)
+        card.frames[i].bowl(10);
+        card.frames[i].bowl(0);
       }
-      card.getFinalFrameBonus(10)
-      card.getFinalFrameBonus(10)
+      card.getFinalFrameBonus(10);
+      card.getFinalFrameBonus(10);
       expect(card.getTotalScore()).toEqual(300);
-    })
-  })
+    });
+  });
 
-  describe('a normal game', function(){
-    it('shows the total score', function(){
+  describe('a normal game', () => {
+    it('shows the total score', () => {
       for (let i = 0; i < 9; i++) {
-        card.frames[i].bowl(6)
-        card.frames[i].bowl(0)
+        card.frames[i].bowl(6);
+        card.frames[i].bowl(0);
       }
 
-      card.frames[9].bowl(7)
-      card.frames[9].bowl(3)
+      card.frames[9].bowl(7);
+      card.frames[9].bowl(3);
 
-      card.getFinalFrameBonus(5)
+      card.getFinalFrameBonus(5);
       expect(card.getTotalScore()).toEqual(69);
-    })
-  })
-})
+    });
+  });
+});

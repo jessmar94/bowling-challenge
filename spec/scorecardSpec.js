ca117380cfc45d182d848233ae0fb5153aed4d69
Jessica Marais <jessica_marais@hotmail.com>
2020-01-02 16:00:53 +0000

    Update scorecard spec

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -1,29 +1,19 @@
-"use strict";
+const Frame = require('../src/frame');
+const Scorecard = require('../src/scorecard');
 
 describe("Scorecard", function() {
-  var card
+  let card
   var frame
 
   beforeEach(function() {
-    card = new Scorecard;
-    frame = new Frame;
-  })
-
-  describe('initFrames', function() {
-    it('pushes a frame into the frames hash', function() {
-      frame = [5,2]
-      card.initFrames(frame)
-      expect(card.frames).toEqual({ 1:[5, 2], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[], 10:[]})
-    })
+    card = new Scorecard(Frame);
   })
 
   describe('gutter game', function() {
     it('returns a total score of 0 for gutter game', function() {
-      let frames = []
-      for (var i = 0; i < 10; i++) {
-        frames[i] = new Frame()
-        frames[i].bowl(0)
-        frames[i].bowl(0)
+      for (let i = 0; i < 10; i++) {
+        card.frames[i].bowl(0)
+        card.frames[i].bowl(0)
       }
       for (var i = 0; i < 10; i++) {
         card.initFrames(frames[i])
@@ -34,14 +24,10 @@ describe("Scorecard", function() {
 
   describe('total score', function() {
     it('returns total score when no strike or spare', function() {
-      let frames = []
-      for (var i = 0; i < 10; i++) {
-        frames[i] = new Frame()
-        frames[i].bowl(4)
-        frames[i].bowl(4)
-      }
-      for (var i = 0; i < 10; i++) {
-        card.initFrames(frames[i])
+      for (let i = 0; i < 10; i++) {
+        card.frames[i] = new Frame()
+        card.frames[i].bowl(4)
+        card.frames[i].bowl(4)
       }
       expect(card.getTotalScore()).toEqual(80)
     })
@@ -49,38 +35,26 @@ describe("Scorecard", function() {
 
   describe('a perfect game', function(){
     it('shows a total score of 300 for a perfect game', function(){
-      let frames = [];
-      for (var i = 0; i < 10; i++) {
-        frames[i] = new Frame();
-        frames[i].bowl(10)
-        frames[i].bowl(0)
-      }
-      for (var i = 0; i < 10; i++){
-        card.initFrames(frames[i])
+      for (let i = 0; i < 10; i++) {
+        card.frames[i].bowl(10)
+        card.frames[i].bowl(0)
       }
       card.getFinalFrameBonus(10)
-      card.lastBonus(10)
+      card.getFinalFrameBonus(10)
       expect(card.getTotalScore()).toEqual(300);
     })
   })
 
   describe('a normal game', function(){
     it('shows the total score', function(){
-      let frames = [];
-      for (var i = 0; i < 9; i++) {
-        frames[i] = new Frame();
-        frames[i].bowl(6)
-        frames[i].bowl(0)
+      for (let i = 0; i < 9; i++) {
+        card.frames[i].bowl(6)
+        card.frames[i].bowl(0)
       }
 
-      let frame10 = new Frame();
-      frame10.bowl(7)
-      frame10.bowl(3)
-      frames.push(frame10)
+      card.frame10.bowl(7)
+      card.frame10.bowl(3)
 
-      for (var i = 0; i < 10; i++){
-        card.initFrames(frames[i])
-      }
       card.getFinalFrameBonus(5)
       expect(card.getTotalScore()).toEqual(69);
     })
--- src/scorecard.js
+++ src/scorecard.js
@@ -3,7 +3,6 @@
 function Scorecard(frameType) {
   this.frames = [];
   this.bonusFrame = [];
-  this.bonusScore = 0;
   this.finalFrameBonus = [];
   this.gameOver = false;
   this.initFrames(FrameType);
@@ -13,11 +12,11 @@ Scorecard.prototype.initFrames = function(frame){
   for (let i = 0; i < 10; i++) {
     this.frames.push(new FrameType());
   }
-}
+};
 
 Scorecard.prototype.getCumulativeScore = function() {
   return this.frames.reduce((total, frame) => total + frame.getFrameScore(), 0);
-}
+};
 
 // for first 8 frames
 Scorecard.prototype.calcFrameBonus = function() {
@@ -30,19 +29,20 @@ Scorecard.prototype.calcFrameBonus = function() {
     }
   }
   this.getPenultimateFrameBonus();
-}
+};
 
 // for 9th frame
 Scorecard.prototype.getPenultimateFrameBonus = function() {
-  if (this.frames[9].strike) {
-    this.bonusFrame[9] = (this.frames[9].frameScore || 0) + (this.frames[10].frameScore || 0)
-  } else if (this.frames[9].spare) {
-    this.bonusFrame[9] = this.frames[9][0]
+  if (this.frames[8].isStrike()) {
+    this.bonusFrame[8] = (this.frames[8].getFrameScore()) + (this.frames[9].getFrameScore())
+  } else if (this.frames[8].isSpare()) {
+    const [spareScore = 0] = this.frames[8].getRolls();
+    this.bonusFrame[8] = spareScore;
   }
-}
+};
 
-// for 10th frame
-Scorecard.prototype.getFinalFrameBonus = function(pins) {
+// for 9th frame
+Scorecard.prototype.getPenultimateFrameBonus = function(pins) {
   if (pins > 10){
     console.log('You cannot knock more than 10 pins')
   } else if (this.frames[10].strike || this.frames[10].spare) {
@@ -53,23 +53,29 @@ Scorecard.prototype.getFinalFrameBonus = function(pins) {
       this.finalFrameBonus.push(pins)
     }
   }
-}
+};
 
-Scorecard.prototype.lastBonus = function(pins) {
-  this.finalFrameBonus.push(pins)
-}
+// for 10th frame
+Scorecard.prototype.getFinalFrameBonus = function (pins) {
+  if (pins > 10) {
+    // eslint-disable-next-line no-console
+    console.log('You cannot knock more than 10 pins');
+    return;
+  }
+  if (this.frames[9].isStrike() || this.frames[9].isSpare()) {
+    this.finalFrameBonus.push(pins);
+  }
+};
 
 Scorecard.prototype.calcFinalFrameBonus = function() {
-  return this.finalFrameBonus.reduce((a, b) => a + b, 0);
-}
+  return this.finalFrameBonus.reduce((total, next) => total + next, 0);
+};
 
 Scorecard.prototype.calcBonusScore = function() {
-  for (var i = 1; i < 11; i++){
-    this.bonusScore += this.bonusFrame[i];
-  }
-  return this.bonusScore;
-}
+  return this.bonusFrame.reduce((total, next) => total + next, 0);
+};
 
 Scorecard.prototype.getTotalScore = function() {
+  this.calcFrameBonus();
   return this.getCumulativeScore() + this.calcBonusScore() + this.calcFinalFrameBonus();
-}
+};
